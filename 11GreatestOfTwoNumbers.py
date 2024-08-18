090cc557d2ab7c0af7c47bc6603b1aba8a179eae
Pratik Khose <pratikkhose13@gmail.com>
2024-08-18 13:02:46 +0530

    done11

---
+++ 11GreatestOfTwoNumbers.py
@@ -0,0 +1,8 @@
+# This is the 11GreatestOfTwoNumbers.py file
+Number1 = int(input('Enter number one : '))
+Number2 = int(input('Enter number two : '))
+
+if Number1>Number2:
+    print(Number1,'is greater')
+else:
+    print(Number2, 'is greater')
